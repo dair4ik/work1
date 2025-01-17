efa7bb26d583f76e54c89e952bed1e5cdeadffb7
dair4ik <dairkal2006@gmail.com>
2025-01-17 16:11:02 +0500

    1stCommit

---
+++ t2.py
@@ -0,0 +1,11 @@
+#w3school(Syntax)1st
+
+if 5 > 2:
+    print("Five greater than two")
+#w3school(Syntax)2nd
+
+x=5
+y="Hello, World!"
+#w3school(Syntax)3rd
+
+#This is a comment
---
+++ t3.py
@@ -0,0 +1,21 @@
+#w3school(Comments)1st
+
+print("Hello, World!") #This is a comment
+#w3school(Comments)2nd
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+#w3school(Multiline Comments)3rd
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#w3school(Multiline Comments)4th
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
